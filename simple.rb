6c9b6d670c78d54498f5f96a9dcc677a63695091
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-02-16 16:50:46 +0900

    modify Machine to deal with statement. add If

--- simple.rb
+++ simple.rb
@@ -156,16 +156,43 @@ class Assign < Struct.new(:name, :expression)
   end
 end
 
-class Machine < Struct.new(:expression, :environment)
+class If < Struct.new(:condition, :consequence, :alternative)
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if condition.reducible?
+      [If.new(condition.reduce(environment), consequence, alternative), environment]
+    else
+      case condition
+      when Boolean.new(true)
+        [consequence, environment]
+      when Boolean.new(false)
+        [alternative, environment]
+      end
+    end
+  end
+end
+
+class Machine < Struct.new(:statement, :environment)
   def step
-    self.expression = expression.reduce(environment)
+    self.statement, self.environment = statement.reduce(environment)
   end
 
   def run
-    while expression.reducible?
-      puts expression
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
       step
     end
-    puts expression
+    puts "#{statement}, #{environment}"
   end
 end
