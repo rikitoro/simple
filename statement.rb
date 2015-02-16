9cd486741617a9ace8df666c0424ffbfe67e9a77
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-02-16 17:07:16 +0900

    divide simple.rb to expression.rb, boolean.rb, variable.rb, statement.rb, and simple.rb

--- statement.rb
+++ statement.rb
@@ -1,61 +1,3 @@
-require_relative './expression'
-
-class Boolean < Struct.new(:value)
-  def to_s
-    value.to_s
-  end
-
-  def inspect
-    "<<#{self}>>"
-  end
-
-  def reducible?
-    false
-  end
-end
-
-class LessThan < Struct.new(:left, :right)
-  def to_s
-    "#{left} < #{right}"
-  end
-
-  def inspect
-    "<<#{self}>>"
-  end
-
-  def reducible?
-    true
-  end
-
-  def reduce(environment)
-    if left.reducible?
-      LessThan.new(left.reduce(environment), right)
-    elsif right.reducible?
-      LessThan.new(left, right.reduce(environment))
-    else
-      Boolean.new(left.value < right.value)      
-    end
-  end
-end
-
-class Variable < Struct.new(:name)
-  def to_s
-    name.to_s
-  end
-
-  def inspect
-    "<<#{self}>>"
-  end
-  
-  def reducible?
-    true
-  end
-
-  def reduce(environment)
-    environment[name]
-  end
-end
-
 class DoNothing
   def to_s
     'do_nothing'
@@ -120,19 +62,5 @@ class If < Struct.new(:condition, :consequence, :alternative)
         [alternative, environment]
       end
     end
-  end
-end
-
-class Machine < Struct.new(:statement, :environment)
-  def step
-    self.statement, self.environment = statement.reduce(environment)
-  end
-
-  def run
-    while statement.reducible?
-      puts "#{statement}, #{environment}"
-      step
-    end
-    puts "#{statement}, #{environment}"
   end
 end
