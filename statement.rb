b7690ceade488d206cba19cd80d30da9cbb38430
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-02-16 17:42:37 +0900

    add While

--- statement.rb
+++ statement.rb
@@ -87,5 +87,22 @@ class Sequence < Struct.new(:first, :second)
       [Sequence.new(reduced_first, second), reduced_environment]
     end
   end
-  
+end
+
+class While < Struct.new(:condition, :body)
+  def to_s
+    "while (#{condition}) { #{body} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    [If.new(condition, Sequence.new(body, self), DoNothing.new), environment]
+  end
 end
