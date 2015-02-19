d4a98214542d2bb57788bb96f584e08f0c9d0b4a
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-02-19 11:08:14 +0900

    add #to_ruby to While, Sequence, and If

--- statements.rb
+++ statements.rb
@@ -88,6 +88,13 @@ class If < Struct.new(:condition, :consequence, :alternative)
       alternative.evaluate(environment)
     end 
   end
+
+  def to_ruby
+    "-> e { if (#{condition.to_ruby}).call(e)" + 
+    " then (#{consequence.to_ruby}).call(e)" +
+    " else (#{alternative.to_ruby}).call(e)" +
+    " end }"
+  end
 end
 
 class Sequence < Struct.new(:first, :second)
@@ -116,6 +123,10 @@ class Sequence < Struct.new(:first, :second)
   def evaluate(environment)
     second.evaluate(first.evaluate(environment))
   end
+
+  def to_ruby
+    "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
+  end
 end
 
 class While < Struct.new(:condition, :body)
@@ -142,5 +153,12 @@ class While < Struct.new(:condition, :body)
     when Boolean.new(false)
       environment
     end
+  end
+
+  def to_ruby
+    "-> e {" +
+    " while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
+    " e" +
+    " }"
   end
 end
